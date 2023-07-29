0df8bbeb48598a0f2a91a8a56a7c364fddd22599
VikramSinghIneuron <vikram0701@yahoo.com>
2023-07-29 11:17:06 +0530

    Second commit

--- app.py
+++ app.py
@@ -1,11 +1,28 @@
-from flask import Flask, request, render_template
+from flask import Flask, request, render_template, jsonify
+import json
+obj = Flask(__name__)
 
-app = Flask(__name__)
-
-@app.route('/')
+@obj.route('/')
 def welcome():
     return "Welcome to Flask."
 
+@obj.route('/cal',methods=["GET"])
+def math_operator():
+    operation= request.json["operation"]
+    number1= int(request.json["number1"])
+    number2=int(request.json["number2"])
+
+    if operation =="add":
+        result = number1 + number2
+    elif operation == "multiply":
+        result = number1*number2
+    elif operation == "divide":
+        result = number1/number2
+    else:
+        result = number1-number2
+
+    #return jsonify(result)
+    return "The operaton is {} and the result is {}".format(operation, result)
 
 if __name__== '__main__':
-    app.run()
+    obj.run(debug=True)
